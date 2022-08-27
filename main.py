c44b4901e6d51e23f17fc237f4ddf69ee544dbfd
Kanmi <kanmi@konfirmed.com>
2022-08-26 21:59:41 -0400

    print works but not writer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
-from csv import writer
+import csv
+from itertools import zip_longest
 
 url = ("https://blackbusinessdirect.ca/listings/british-columbia/vancouver-2")
 page = requests.get(url)
@@ -8,41 +9,23 @@ page = requests.get(url)
 # print(html_text)
 soup = BeautifulSoup(page.content, 'html.parser')
 job = soup.find('div', class_='container-fluid item-list')
-container = soup.find_all('div', class_='row list-item mb-4 mx-3 mx-sm-0')[0]
-list_items = container.find_all('div', 'col-sm-7 px-sm-3 py-3 pt-sm-0 pl-sm-4')
-with open('blackbusinessesincanada.csv', 'w', encoding='utf8', newline='') as f:
-    thewriter = writer(f)
-    header = ['Company', 'Address', 'Tel', 'Url']
-    thewriter.writerow(header)
-for list_item in list_items:
+job_elements = job.find_all("div", class_="row list-item mb-4 mx-3 mx-sm-0")
+for job_element in job_elements:
+    name = job_element.find("h4", class_="mb-2").text.replace('\t', '').replace('\n','')
+    description = job_element.find("div", class_="card-text mb-2").text.replace('\t', '').replace('\n','')
+    location = job_element.find("div", class_="address flex-grow-1").text.replace('\t', '').replace('\n','').replace('BC', 'BC ').replace('604-', ' 604-').replace('(604', ' (604').replace('1-778', ' 1-778').replace('778', ' 778')
+    # telephone = job_element.find("div", class_="tel").text.get('href')
+    website = job_element.find("a", class_="text-green").get('href')
+    header = ['Name','Description','Address','Url']
+    info = [name, description, location, website]
+    # with open('blackbusinessesincanada.csv', 'w', encoding='utf8', newline='') as f:
+    #     writer = csv.writer(f)
+    #     writer.writerow(header)
+    #     writer.writerows(info)
 
-    company_name = list_item.find('div', class_='flex-grow-1')
-    # location = job.find('div', class_='address flex-grow-1')
-    location = list_item.find('div', class_='address flex-grow-1')
-    contact = list_item.find('div', class_='tel')
-    website = list_item.find('h4', class_='mb-2')
-info = [company_name.text.replace('\t',''), location.text.replace('\t',''), contact.text.replace('\t',''), website.a.get('href')]
-thewriter.writerow(info)
-# print(info)
-#     title = list.find('div', class_='')
-# print('''
-# Company Name: {company_name}
-# Address: {location}
-# Tel: {contact}
-# Url: {website}
-# ''')
-# print(company_name.text)
-# print(location.text)
-# print(contact.text)
-# print(website.a.get('href'))
-# url="https://blackbusinessdirect.ca/listings/british-columbia/vancouver-2"
-# page = requests.get(url)
-# print(page)
-# soup = BeautifulSoup(page.content, 'html.parser')
-# lists = soup.find_all('div', class_="listText")
-# for list in lists:
-#     title = list.find('span', class_="itemName").text
-    # location = list.find('span', class_="buttonText")
-    # website = list.find('a', 'href', class_="biglink clearfix")
-    # info = [title]
-    # print(info)
+    print(name)
+    print(description)
+    print(location)
+    # print(telephone)
+    print(website)
+    print()
