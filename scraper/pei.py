6683e727327b5afecbe5f81e3af665bbaf3367f1
Kanmi <kanmi@konfirmed.com>
2022-08-27 03:24:53 -0400

    clean up

--- scraper/pei.py
+++ scraper/pei.py
@@ -9,19 +9,17 @@ job_elements = job.find_all("div", class_="row list-item mb-4 mx-3 mx-sm-0")
 for job_element in job_elements:
     name = job_element.find("h4", class_="mb-2").text.replace('\t', '').replace('\n','')
     description = job_element.find("div", class_="card-text mb-2").text.replace('\t', '').replace('\n','')
-    location = job_element.find("div", class_="address flex-grow-1").text.replace('\t', '').replace('\n','').replace('BC', 'BC ').replace('604-', ' 604-').replace('(604', ' (604').replace('1-778', ' 1-778').replace('778', ' 778')
-    industry = job_element.find("div", class_="cat-name-figure rounded p-2")
-    website = job_element.find("a", class_="text-green").get('href')
-    header = ['Name','Description','Address','Url']
+    location = job_element.find("div", class_="address flex-grow-1").text.replace('\t', '').replace('\n','').replace('902-3147747', '').replace('PEI - ', '').replace('PE', 'PEI').replace('charlottetown, C', 'C')
     telephone = soup.find("div", {"class": "tel"}).a.__getitem__('href')
-    info = [name, description, location, telephone, industry, website]
+    industry = job_element.find("span", class_="cat-name-figure rounded p-2").text
+    header = ['Name', 'Description', 'Address', 'Telephone', 'Industry']
+
+    info = [name, description, location, telephone, industry]
 
     print('*------------*')
     print(name)
     print(description)
     print(location)
-    print(industry)
     print(telephone)
+    print(industry)
     print('*------------*')
-
-    # print(website)
